1e6a7bc0da4b71f3f4b64a79d4239e478955ddd7
ignacio-alorre <ignacio.alorre@gmail.com>
2023-10-19 18:02:48 +0400

    Update InspectingDataframe.py

--- Pandas/InspectingDataframe.py
+++ Pandas/InspectingDataframe.py
@@ -8,3 +8,47 @@ dogs = pd.DataFrame({
 })
 
 print(dogs)
+'''
+    name     breed weight
+0  Pinon    Mastin     30
+1  Trufa   Shepard     16
+2   Scot  Labrador     23
+3   Dama  Labrador     20
+'''
+
+print(dogs.head())
+'''
+    name     breed weight
+0  Pinon    Mastin     30
+1  Trufa   Shepard     16
+2   Scot  Labrador     23
+3   Dama  Labrador     20
+'''
+
+print(dogs.info())
+'''
+RangeIndex: 4 entries, 0 to 3
+Data columns (total 3 columns):
+ #   Column  Non-Null Count  Dtype 
+---  ------  --------------  ----- 
+ 0   name    4 non-null      object
+ 1   breed   4 non-null      object
+ 2   weight  4 non-null      object
+dtypes: object(3)
+memory usage: 224.0+ bytes
+None
+'''
+
+print(dogs.shape)
+'''
+(4, 3)
+'''
+
+print(dogs.describe())
+'''
+         name     breed weight
+count       4         4      4
+unique      4         3      4
+top     Trufa  Labrador     20
+freq        1         2      1
+'''
