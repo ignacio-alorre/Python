a48b8ec1314dca818fdc620cac3043833e501929
ignacio-alorre <ignacio.alorre@gmail.com>
2023-10-19 17:58:49 +0400

    Inspecting DataFrame

---
+++ Pandas/InspectingDataframe.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+dogs = pd.DataFrame({
+    'name' : ['Pinon', 'Trufa', 'Scot', 'Dama'] ,
+    'breed' : ['Mastin', 'Shepard', 'Labrador', 'Labrador'] ,
+    'weight' : ['30', '16', '23', '20']
+})
+
+print(dogs)
